caeeebd9ef43a2981448a80caf12879d75add00c
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-10-31 12:05:49 +0100

    up

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,8 @@
 export default defineAppConfig({
-    variables: {
-      key1: "I use a sentence with ${key2}",
-      key2: "Key 2 value"
+    global: {
+        variables: {
+          key1: "I use a sentence with ${key2}",
+          key2: "Key 2 value"
+        }
     }
   })
